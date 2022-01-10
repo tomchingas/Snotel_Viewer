5ecf5eecfd8a0146efe2e21d151329585a4e7df6
tomchingas <tomchingas@gmail.com>
2022-01-10 15:06:32 -0700

    add view and index.html file to display 6 nearest snotel stations to boise

--- SnotelViewer/SnotelViewer/settings.py
+++ SnotelViewer/SnotelViewer/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'SnotelViewer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- SnotelViewer/stations/views.py
+++ SnotelViewer/stations/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+from .models import Station
 
-# Create your views here.
+# View to get the 6 nearest snotel station locations
+
+# Define user location
+# Hard coded to Boise ID for now, will add dynamic location input later
+longitude = -116.208710
+latitude = 43.603600
+
+user_location = Point(longitude, latitude, srid=4326)
+
+# Add view class for nearby Snotel stations
+class Home(generic.ListView):
+    model = Station
+    context_object_name = 'stations'
+    queryset = Station.objects.annotate(distance=Distance('location',
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'stations/index.html'
+
